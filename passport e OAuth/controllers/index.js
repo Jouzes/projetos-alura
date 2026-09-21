b61d92a8c15ef3d9809d73fa0d94ed5dcd8e0dfa
JMA <josemarceloandradealmeidas@gmail.com>
2026-09-21 20:49:39 -0300

    aula 4 pt1 logout

--- passport e OAuth/controllers/index.js
+++ passport e OAuth/controllers/index.js
@@ -1,56 +1,65 @@
-import { User } from "../models/user"
+import { User } from "../models/user";
 import bcrypt from "bcrypt";
 
 exports.showIndex = (req, res, next) => {
-    res.render('index')
-}
+  res.render("index");
+};
 
 exports.showPageSignUp = (req, res, next) => {
-    res.render('signUp')
-}
+  res.render("signUp");
+};
 
 exports.showMembersPage = (req, res) => {
-    res.render('members')
-}
+  res.render("members");
+};
 
 exports.get404Page = (req, res, next) => {
-    res.status(404).render('404')
-}
+  res.status(404).render("404");
+};
 
 exports.signup = async (req, res, next) => {
-    const {username, email, password} = req.body;
-    const password_hash = await bcrypt.hash(password, 6)
-    const user = new User(username, email, password_hash);
-    try {
-        await user.save();
-        res.redirect("/members");
-    } catch(err) {
-        console.log(err);
-        res.redirect("signup");
-    }
-}
+  const { username, email, password } = req.body;
+  const password_hash = await bcrypt.hash(password, 6);
+  const user = new User(username, email, password_hash);
+  try {
+    await user.save();
+    res.redirect("/members");
+  } catch (err) {
+    console.log(err);
+    res.redirect("signup");
+  }
+};
 
 exports.login = async (req, res, next) => {
-    const {email, password} = req.body;
-    const user = await User.findOne(email, password);
-    try {
-        if (user) {
-            res.redirect("/members");
-        } else {
-            res.render("index");
-        }
-    } catch (err) {
-        console.log(err);
-        res.render("index");
+  const { email, password } = req.body;
+  const user = await User.findOne(email, password);
+  try {
+    if (user) {
+      req.session.user = user;
+      res.redirect("/members");
+    } else {
+      res.render("index");
     }
-}
+  } catch (err) {
+    console.log(err);
+    res.render("index");
+  }
+};
 
 exports.checkAuth = async (req, res, next) => {
-    const auth = false;
-    
-    if (auth) {
-        next()
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
+
+exports.logout = async (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
     } else {
-        res.redirect("/");
+      res.redirect("/");
     }
-}
+  });
+};
--- passport e OAuth/routes/routes.js
+++ passport e OAuth/routes/routes.js
@@ -1,15 +1,25 @@
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
-const controller = require('../controllers/index')
+const sessionStore = require("../util/sessionStorage");
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const controller = require("../controllers/index");
+const session = require("express-session");
 
-router.use(bodyParser.urlencoded({ extended: true }))
+router.use(
+  session({
+    secret: "ausuhaskpds",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  }),
+) / router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', controller.showIndex)
-router.post("/", controller.login)
-router.get('/signup', controller.showPageSignUp)
-router.post('/signup', controller.signup)
-router.get('/members', controller.checkAuth, controller.showMembersPage)
-router.use(controller.get404Page)
+router.get("/", controller.showIndex);
+router.post("/", controller.login);
+router.get("/signup", controller.showPageSignUp);
+router.post("/signup", controller.signup);
+router.get("/members", controller.checkAuth, controller.showMembersPage);
+router.get("/logout", controller.logout);
+router.use(controller.get404Page);
 
-module.exports = router
+module.exports = router;
